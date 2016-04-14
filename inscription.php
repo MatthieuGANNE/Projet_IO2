20cd7dd6ffbb16151b9bccdfd1352503ac50c80d
Firalon <floszc@orange.fr>
2016-04-14 10:21:22 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -3,12 +3,13 @@
 function inscription(){
 	if (!isset($_SESSION["nom"])){
 		return  "<form action=\"index.php?page=sauvegarde\" method=\"post\">
-                    Nom<input type=\"text\" name=\"nom\">   <br/>                 
-                    Prenom<input type=\"text\" name=\"prenom\"><br/>
-                    Date<input type=\"date\" name=\"date\"><br/>
+                    Prenom<input type=\"text\" name=\"prenom\"><br/>                 
+                    Nom<input type=\"text\" name=\"nom\"><br/>
+                    Pseudo<input type=\"text\" name=\"pseudo\"><br/>
                     Email <input type=\"email\" name=\"mail\"><br/>
-					Password<input type=\"password\" name=\"password\">
-        <input type=\"submit\" value=\"Valider  \">
+                    Mot de passe<input type=\"password\" name=\"mdp1\"><br/>
+                    Confirmation de mot de passe<input type=\"password\" name=\"mdp2\"><br/>
+        	    <input type=\"submit\" value=\"Valider  \">
          </form> "  ;
 	}else {
 		$nom=$_SESSION["nom"];
@@ -16,13 +17,13 @@ function inscription(){
                 $date=$_SESSION["date"];
                 $mail=$_SESSION["mail"];
 		return " <form action=\"index.php?page=sauvegarde\" method=\"post\">
-           Nom<input type=\"text\" name=\"nom\" value=\"$nom\">  <br/>                 
-        Prenom<input type=\"text\" name=\"prenom\" value=\"$prenom\"><br/>
-           Date<input type=\"date\" name=\"date\" value=\"$date\"><br/>
-           Email <input type=\"email\" name=\"mail\" value=\"$mail\"><br/>
-<input type = \"submit\" value=\"Enregistrer les modifications\">	
+           		 	Nom<input type=\"text\" name=\"nom\" value=\"$nom\">  <br/>                 
+				Prenom<input type=\"text\" name=\"prenom\" value=\"$prenom\"><br/>
+           			Date<input type=\"date\" name=\"date\" value=\"$date\"><br/>
+           			Email <input type=\"email\" name=\"mail\" value=\"$mail\"><br/>
+				<input type = \"submit\" value=\"Enregistrer les modifications\">	
 
-</form>
+			</form>
 	
 ";
 	}	
